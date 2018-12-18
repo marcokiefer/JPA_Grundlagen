71c9d05540d2d33af0186b1be195fa42f4051ad4
Marco Kiefer <kiefer.marco@me.com>
2018-12-18 09:24:12 +0100

    Verbindung MySQL finish

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("JPA-Grundlagen");
 
 		// Tabelle in Datanbank erzeugen
+		
 		// Verbindung zur Datanbenk
 		String datenbank = "jdbc:mysql://localhost:3306/labor?useSSL=false";
 		String user = "professor";
